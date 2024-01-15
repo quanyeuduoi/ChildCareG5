fcb9a503d31074f190e4ded65ef2c7cdc77be8ec
Le Quang Khai (K17 HL) <khailqhe170154@fpt.edu.vn>
2024-01-15 21:59:08 +0700

    base code with package

---
+++ ChildCareProject/src/java/dao/DAO.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import model.Customer;
+import context.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author Dell
+ */
+public class DAO {
+
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    //Register customer
+    public Customer addCustomer(String paymentID, String username, String password, String fullName, String phoneNumber,
+            String address, String email, String cOTP) {
+        String query = "INSERT INTO [dbo].[Customer]\n"
+                + "           ([PayID]\n"
+                + "           ,[Username]\n"
+                + "           ,[Password]\n"
+                + "           ,[Fullname]\n"
+                + "           ,[Phonenumber]\n"
+                + "           ,[Address]\n"
+                + "           ,[Email]\n"
+                + "           ,[COTP])\n"
+                + "     VALUES\n"
+                + "           (?,?,?,?,?,?,?,?)";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, paymentID);
+            ps.setString(2, username);
+            ps.setString(3, password);
+            ps.setString(4, fullName);
+            ps.setString(5, phoneNumber);
+            ps.setString(6, address);
+            ps.setString(8, email);
+            ps.setString(9, cOTP);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    //Send OTP
+    public void sendOTPEmail(String subjectMail, String toEmail, String otp) {
+        final String fromEmail = "pchildcareg5@gmail.com";
+        final String password = "eesx dgaj vcox vrjw";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(fromEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subjectMail);
+            message.setText("Your OTP code is: " + otp + ". It will expired after 5 minutes.");
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //Check Customer Exist
+    public Customer CheckCustomerExist(String Email) {
+        String query = "select * from Customer where [Email] = ?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, Email);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Customer(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getBoolean(5),
+                        rs.getString(6),
+                        rs.getDate(7),
+                        rs.getDouble(8),
+                        rs.getString(9),
+                        rs.getString(10),
+                        rs.getString(11),
+                        rs.getInt(12),
+                        rs.getString(13)
+                );
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+}
--- ChildCareProject/web/LoginRegister.jsp
+++ ChildCareProject/web/LoginRegister.jsp
@@ -13,43 +13,67 @@
         <link href="assets/css/form.css" rel="stylesheet">
     </head>
     <h2>Welcome to Child Care</h2>
-        <div class="container" id="container">
-            <div class="form-container sign-up-container">
-                <form action="#">
-                    <h1>Create Account</h1>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button>Sign Up</button>
-                </form>
-            </div>
-            <div class="form-container sign-in-container">
-                <form action="#">
-                    <h1>Sign in</h1>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <a href="#">Forgot your password?</a>
-                    <button>Sign In</button>
-                </form>
-            </div>
-            <div class="overlay-container">
-                <div class="overlay">
-                    <div class="overlay-panel overlay-left">
-                        <h1>Welcome Back!</h1>
-                        <p>To keep connected with us please login with your personal info</p>
-                        <button class="ghost" id="signIn">Sign In</button>
-                    </div>
-                    <div class="overlay-panel overlay-right">
-                        <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details and start journey with us</p>
-                        <button class="ghost" id="signUp">Sign Up</button>
-                    </div>
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="#">
+                <h1>Create Account</h1>
+                <input type="text" name="fullName" placeholder="Name" />
+                <div class="email-container">
+                    <input name="email" type="email" id="email" placeholder="Email" />
+                    <button class="small-button" onclick="sendOTP()">Send OTP</button>
+                </div>
+                <input type="password" name="password" placeholder="Password" />
+                <input type="rePassword" name="rePassword" placeholder="Re-Password" />
+                <input type="otp" name="cOTP" placeholder="Enter OTP here" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="#">
+                <h1>Sign in</h1>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button class="ghost" id="signUp">Sign Up</button>
                 </div>
             </div>
         </div>
+    </div>
 
 
-        <!-- Vendor JS Files -->
-        <script src="assets/js/login.js"></script>
-    </body>
+    <!-- Vendor JS Files -->
+    <script src="assets/js/login.js"></script>
+    <style>
+        .email-container {
+            display: flex;
+            align-items: center;
+        }
+
+        .email-container input {
+            margin-right: 10px; 
+            width: 90%; 
+        }
+
+        .small-button {
+            font-size: 10px; 
+            width: 10%; 
+            align-items: center;
+        }
+    </style>
+
+
+</body>
 </html>
