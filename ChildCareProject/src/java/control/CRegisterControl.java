050d7213a282ea04fff18a9f4410ed33fa8c0138
Le Quang Khai (K17 HL) <khailqhe170154@fpt.edu.vn>
2024-01-17 01:08:01 +0700

    fix error

--- ChildCareProject/src/java/control/CRegisterControl.java
+++ ChildCareProject/src/java/control/CRegisterControl.java
@@ -58,6 +58,7 @@ public class CRegisterControl extends HttpServlet {
         request.getRequestDispatcher("LoginRegister.jsp").forward(request, response);
     }
 
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
