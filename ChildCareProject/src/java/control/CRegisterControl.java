848538306b884525aaa8d8883c8aea5a5fc5d6c8
NguyenTuanVinhNinhHE172162 <ninhntvhe172162@fpt.edu.vn>
2024-01-19 13:24:51 +0700

    ForgetPass chua hoan chinh

--- ChildCareProject/src/java/control/CRegisterControl.java
+++ ChildCareProject/src/java/control/CRegisterControl.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package control;
 
+import dao.DAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -12,37 +12,63 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Customer;
 
 /**
  *
  * @author Dell
  */
-@WebServlet(name="CRegisterControl", urlPatterns={"/register"})
+@WebServlet(name = "CRegisterControl", urlPatterns = {"/register"})
 public class CRegisterControl extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String fullName = request.getParameter("fullName");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String rePassword = request.getParameter("rePassword");
-        String paymentID = null;
         String address = null;
         String phoneNumber = null;
         String cOTP = null;
-    } 
+        
+        DAO dao = new DAO();
+        if (!password.equals(rePassword)) {
+            // Display an error message or handle the error
+            request.setAttribute("message", "Password and confirm password do not match.");
+            request.getRequestDispatcher("LoginRegister.jsp").forward(request, response);
+            return;
+        }
+        Customer cus = dao.CheckCustomerExist(email);
+        if(cus!=null){
+            request.setAttribute("message", "Register failed. Emal existed.");
+            request.getRequestDispatcher("LoginRegister.jsp").forward(request, response);
+//        boolean isCOTPValid = dao.isCOTPValid(cOTP);
+//        if (!isCOTPValid) {
+//        request.setAttribute("message", "Your OTP is incorrect.");
+//        request.getRequestDispatcher("LoginRegister.jsp").forward(request, response);
+    }else {
+        dao.addCustomer(email, password, fullName, phoneNumber, address, cOTP);
+        request.setAttribute("message", "Register successfully.");
+        request.getRequestDispatcher("LoginRegister.jsp").forward(request, response);
+    }
+
+        
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -50,12 +76,13 @@ public class CRegisterControl extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -63,12 +90,13 @@ public class CRegisterControl extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
--- ChildCareProject/src/java/dao/CustomerDAO.java
+++ ChildCareProject/src/java/dao/CustomerDAO.java
@@ -3,13 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
-
+import context.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Customer;
 
 /**
  *
  * @author Dell
  */
 public class CustomerDAO {
-    
-    
+     Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+     public void forGetPass(String password,String email) {
+        try {
+            String strSQL="update Products "
+                    + "set Password="+password+" "
+                    + "where Email="+email+"";
+            conn = new DBContext().getConnection();
+            ps=conn.prepareStatement(strSQL);
+            ps.setString(1, password);
+            ps.setDouble(2, Double.parseDouble(email));
+            
+            rs = ps.executeQuery();
+            
+        } catch (Exception e) {
+            System.out.println("UpdateProduct:"+e.getMessage());
+           
+        }
+    }
 }
